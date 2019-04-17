9805f533ff23037d04502ddead95b6ecca5853a2
bruno <bruno@debianPdVMClient>
2019-04-17 11:02:52 +0100

    Listar objetivos semi completo

--- cricketWeb/src/java/controller/GoalBean.java
+++ cricketWeb/src/java/controller/GoalBean.java
@@ -6,7 +6,13 @@
 package controller;
 
 import BridgeLogicController.BridgeLocal;
+import Utils.Config;
+import cricketdto.GoalDTO;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -22,6 +28,30 @@ public class GoalBean implements Serializable{
     
     
     public GoalBean() {
+    }
+    
+    public List<GoalDTO> getAllGoals() {
+        try {
+            List<GoalDTO> goals = new ArrayList();
+            
+            GoalDTO goal = new GoalDTO();
+            goal.setName("Goal example");
+            goal.setDesc("goal desc");
+            goal.setFinalValue(12);
+            goal.setTotalValue(20);
+            goal.setType(Config.POSITIVE);
+            goal.setStatus(Config.RECURRENT);            
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");   
+            Date finalDate = formatter.parse("17/04/2019");
+            goal.setFinalDate(finalDate);
+            
+            goals.add(goal);
+            goals.add(goal);
+
+            return goals;
+        } catch (Exception ex) {
+            return new ArrayList();
+        }
     }
     
     
