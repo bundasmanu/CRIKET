6f3aca9875994896acfae7e4560b551dd33e8d78
stefan <stefan@CliPD19.stefan>
2019-05-01 20:13:49 +0100

    Commit Dia do trabalhador

--- cricketDTO/src/cricketdto/CategoryDTO.java
+++ cricketDTO/src/cricketdto/CategoryDTO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cricketdto;
+package cricketdto; 
 
 import java.io.Serializable;
 import java.util.Collection;
